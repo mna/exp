e4806a7a3b9893c91789e4a0d217ad97c0750f68
Martin Angers <martin.n.angers@gmail.com>
2015-03-17 19:53:53 -0400

    more work on builder

--- peg/builder/builder.go
+++ peg/builder/builder.go
@@ -1,11 +1,19 @@
+// Package builder generates the parser code for a given grammar. It makes
+// no attempt to verify the correctness of the grammar.
 package builder
 
 import (
 	"fmt"
 	"io"
+	"strconv"
 
 	"github.com/PuerkitoBio/exp/peg/ast"
 )
+
+var funcTemplate = `func %s(%s) (interface{}, error) {
+%s
+}
+`
 
 func BuildParser(w io.Writer, g *ast.Grammar) error {
 	b := &builder{w: w}
@@ -13,13 +21,21 @@ func BuildParser(w io.Writer, g *ast.Grammar) error {
 }
 
 type builder struct {
-	w   io.Writer
-	err error
+	w         io.Writer
+	err       error
+	ruleName  string
+	exprIndex int
+	argsStack []map[string]interface{}
 }
 
 func (b *builder) buildParser(g *ast.Grammar) error {
 	b.writePackage(g.Package)
 	b.writeInit(g.Init)
+
+	for _, rule := range g.Rules {
+		b.writeRule(rule)
+	}
+
 	return b.err
 }
 
@@ -38,6 +54,95 @@ func (b *builder) writeInit(init *ast.CodeBlock) {
 	// remove opening and closing braces
 	val := init.Val[1 : len(init.Val)-1]
 	b.writelnf("%s\n", val)
+}
+
+func (b *builder) writeRule(rule *ast.Rule) {
+	if rule == nil || rule.Name == nil {
+		return
+	}
+
+	// keep trace of the current rule, as the code blocks are created
+	// in functions named "on<RuleName><#ExprIndex>".
+	b.ruleName = rule.Name.Val
+	b.exprIndex = 0
+	b.writeExpr(rule.Expr)
+}
+
+func (b *builder) writeExpr(expr ast.Expression) {
+	b.exprIndex++
+	switch expr := expr.(type) {
+	case *ast.ActionExpr:
+		// TODO : how/when?
+		//b.pushArgsSet()
+		b.writeExpr(expr)
+		b.writeActionExpr(expr)
+		//b.popArgsSet()
+
+	case *ast.AndCodeExpr:
+		// TODO : should be able to access labeled vars too, but when to
+		// start a new args set?
+		b.writeAndCodeExpr(expr)
+
+	case *ast.LabeledExpr:
+		// TODO : add argument to argsset
+		b.writeExpr(expr.Expr)
+
+	case *ast.NotCodeExpr:
+		// TODO : should be able to access labeled vars too, but when to
+		// start a new args set?
+		b.writeNotCodeExpr(expr)
+
+	case *ast.AndExpr:
+		b.writeExpr(expr.Expr)
+	case *ast.ChoiceExpr:
+		for _, alt := range expr.Alternatives {
+			b.writeExpr(alt)
+		}
+	case *ast.NotExpr:
+		b.writeExpr(expr.Expr)
+	case *ast.OneOrMoreExpr:
+		b.writeExpr(expr.Expr)
+	case *ast.SeqExpr:
+		for _, sub := range expr.Exprs {
+			b.writeExpr(sub)
+		}
+	case *ast.ZeroOrMoreExpr:
+		b.writeExpr(expr.Expr)
+	case *ast.ZeroOrOneExpr:
+		b.writeExpr(expr.Expr)
+	}
+}
+
+func (b *builder) writeActionExpr(act *ast.ActionExpr) {
+	if act == nil {
+		return
+	}
+	b.writeFunc(act.Code)
+}
+
+func (b *builder) writeAndCodeExpr(and *ast.AndCodeExpr) {
+	if and == nil {
+		return
+	}
+	b.writeFunc(and.Code)
+}
+
+func (b *builder) writeNotCodeExpr(not *ast.NotCodeExpr) {
+	if not == nil {
+		return
+	}
+	b.writeFunc(not.Code)
+}
+
+func (b *builder) writeFunc(code *ast.CodeBlock) {
+	if code == nil {
+		return
+	}
+	b.writef(funcTemplate, b.funcName(), "", code.Val)
+}
+
+func (b *builder) funcName() string {
+	return "on" + b.ruleName + "_" + strconv.Itoa(b.exprIndex)
 }
 
 func (b *builder) writef(f string, args ...interface{}) {
