50b99f577ec468e882773ad3ab3d3f103ca8e899
Martin Angers <martin.n.angers@gmail.com>
2015-03-18 22:30:20 -0400

    start building a simulated, hand-generated calculator parser

--- peg/parser/calc_test.go
+++ peg/parser/calc_test.go
@@ -1,21 +1,22 @@
 package parser
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
 	"github.com/PuerkitoBio/exp/peg/bootstrap"
 )
 
-var grammar = `package test
+var calcGrammar = `package test
 
 start = additive eof
 additive = left:multiplicative "+" space right:additive / multiplicative
 multiplicative = left:primary "*" space right:multiplicative / primary
 primary = integer / "(" space additive:additive ")" space
-integer "integer" = digits:[0123456789]+ space {integer}
+integer = digits:[0123456789]+ space {integer}
 space = ' '*
-eof = !. { eof }
+eof = !.
 `
 
 var src = `9 + 5 * (1+2)`
@@ -23,14 +24,18 @@ var src = `9 + 5 * (1+2)`
 func TestCalcGrammar(t *testing.T) {
 	// parse the grammar
 	p := bootstrap.NewParser()
-	g, err := p.Parse("", strings.NewReader(grammar))
+	g, err := p.Parse("", strings.NewReader(calcGrammar))
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	res, err := parseUsingAST("", strings.NewReader(src), g)
-	if err != nil {
-		t.Fatal(err)
+	for _, rule := range g.Rules {
+		fmt.Printf("%s\n", rule)
 	}
-	t.Logf("%#v\n", res)
+
+	// res, err := parseUsingAST("", strings.NewReader(src), g)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+	// t.Logf("%#v\n", res)
 }
