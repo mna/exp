a719f88c4136dfc6b38abddc851707d869d951bb
Martin Angers <martin.n.angers@gmail.com>
2016-03-08 21:56:44 -0500

    juggler/broker/redisbroker: test Broker.Result, refactor for common implementation with Call

--- juggler/broker/redisbroker/broker_test.go
+++ juggler/broker/redisbroker/broker_test.go
@@ -15,49 +15,42 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestBrokerCall(t *testing.T) {
+const cap = 2
+
+func testBrokerCallOrRes(t *testing.T, keyFmt string, run func(*Broker, uuid.UUID) (uuid.UUID, error)) {
 	cmd, port := redistest.StartServer(t, nil)
 	defer cmd.Process.Kill()
 
 	pool := redistest.NewPool(t, ":"+port)
 	broker := &Broker{
-		Pool:    pool,
-		LogFunc: logIfVerbose,
-		CallCap: 2,
+		Pool:      pool,
+		LogFunc:   logIfVerbose,
+		CallCap:   cap,
+		ResultCap: cap,
 	}
 
 	var uuids []uuid.UUID
-	connUUID := uuid.NewRandom()
-	key := fmt.Sprintf(callKey, "a")
-	for i := 0; i < 3; i++ {
-		msgUUID := uuid.NewRandom()
-		cp := &msg.CallPayload{
-			ConnUUID: connUUID,
-			MsgUUID:  msgUUID,
-			URI:      "a",
-		}
-		uuids = append(uuids, msgUUID)
-		err := broker.Call(cp, time.Second)
-		if i < 2 {
+	// run all on same key
+	keyUUID := uuid.NewRandom()
+	for i := 0; i <= cap; i++ {
+		uid, err := run(broker, keyUUID)
+		uuids = append(uuids, uid)
+		if i < cap {
 			assert.NoError(t, err, "Call %d", i)
 		} else {
 			assert.Error(t, err, "Call %d", i)
 			assert.Contains(t, err.Error(), "list capacity exceeded", "error has expected message")
-			t.Logf("%T %[1]v", err)
 		}
 	}
 
 	// the first 2 msg uuids should be present, in inverted order (LPUSH)
+	key := fmt.Sprintf(keyFmt, keyUUID)
 	expectUUIDs(t, pool.Get(), key, uuids[1], uuids[0])
 
 	// call on a different URI works fine
-	cp := &msg.CallPayload{
-		ConnUUID: connUUID,
-		MsgUUID:  uuid.NewRandom(),
-		URI:      "b",
-	}
-	err := broker.Call(cp, time.Second)
-	assert.NoError(t, err, "Call on different URI")
+	diffKeyUUID := uuid.NewRandom()
+	_, err := run(broker, diffKeyUUID)
+	assert.NoError(t, err, "Call on different key")
 
 	// popping a value should pop uuids[0]
 	rc := pool.Get()
@@ -67,17 +60,37 @@ func TestBrokerCall(t *testing.T) {
 
 	expectUUIDs(t, pool.Get(), key, uuids[1])
 
-	// call should now work
-	cp = &msg.CallPayload{
-		ConnUUID: connUUID,
-		MsgUUID:  uuid.NewRandom(),
-		URI:      "a",
-	}
-	uuids = append(uuids, cp.MsgUUID)
-	err = broker.Call(cp, time.Second)
+	// call should now work on original key
+	uid, err := run(broker, keyUUID)
+	uuids = append(uuids, uid)
 	assert.NoError(t, err, "Call after RPOP")
 
 	expectUUIDs(t, pool.Get(), key, uuids[3], uuids[1])
+}
+
+func TestBrokerCall(t *testing.T) {
+	connUUID := uuid.NewRandom()
+	testBrokerCallOrRes(t, callKey, func(b *Broker, keyParm uuid.UUID) (uuid.UUID, error) {
+		cp := &msg.CallPayload{
+			ConnUUID: connUUID,
+			MsgUUID:  uuid.NewRandom(),
+			URI:      keyParm.String(),
+		}
+		err := b.Call(cp, time.Second)
+		return cp.MsgUUID, err
+	})
+}
+
+func TestBrokerResult(t *testing.T) {
+	testBrokerCallOrRes(t, resKey, func(b *Broker, keyParm uuid.UUID) (uuid.UUID, error) {
+		rp := &msg.ResPayload{
+			ConnUUID: keyParm,
+			MsgUUID:  uuid.NewRandom(),
+			URI:      "z",
+		}
+		err := b.Result(rp, time.Second)
+		return rp.MsgUUID, err
+	})
 }
 
 func expectUUIDs(t *testing.T, rc redis.Conn, key string, uuids ...uuid.UUID) {
