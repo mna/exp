1d550b1cacc8f9c9c18b6de3f9bf8f36997a5981
Martin Angers <martin.n.angers@gmail.com>
2016-03-10 14:17:37 -0500

    juggler/msg: test marshaling/unmarshaling of all messages

--- juggler/msg/msg_test.go
+++ juggler/msg/msg_test.go
@@ -1,11 +1,75 @@
 package msg
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"reflect"
 	"strings"
 	"testing"
+	"time"
 
+	"github.com/pborman/uuid"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestMarshalUnmarshal(t *testing.T) {
+	call, err := NewCall("a", map[string]interface{}{"x": 3}, time.Second)
+	require.NoError(t, err, "NewCall")
+	pub, err := NewPub("d", map[string]interface{}{"y": "ok"})
+	require.NoError(t, err, "NewPub")
+	rp := &ResPayload{
+		ConnUUID: uuid.NewRandom(),
+		MsgUUID:  uuid.NewRandom(),
+		URI:      "g",
+		Args:     json.RawMessage("null"),
+	}
+	ep := &EvntPayload{
+		MsgUUID: uuid.NewRandom(),
+		Channel: "h",
+		Pattern: "h*",
+		Args:    json.RawMessage(`"string"`),
+	}
+
+	cases := []Msg{
+		call,
+		NewSub("b", false),
+		NewUnsb("c", true),
+		pub,
+		NewErr(call, 500, io.EOF),
+		NewOK(pub),
+		NewRes(rp),
+		NewEvnt(ep),
+	}
+	for i, m := range cases {
+		b, err := json.Marshal(m)
+		require.NoError(t, err, "Marshal %d", i)
+
+		mm, err := Unmarshal(bytes.NewReader(b))
+		require.NoError(t, err, "Unmarshal %d", i)
+
+		// for ErrMsg, the Err is not marshaled, so zero it before the comparison
+		if m.Type() == ErrMsg {
+			m.(*Err).Payload.Err = nil
+		}
+
+		assert.True(t, reflect.DeepEqual(m, mm), "DeepEqual %d", i)
+
+		_, err = UnmarshalRequest(bytes.NewReader(b))
+		assert.Equal(t, m.Type().IsRead(), err == nil, "UnmarshalRequest for %d", i)
+
+		_, err = UnmarshalResponse(bytes.NewReader(b))
+		assert.Equal(t, m.Type().IsWrite(), err == nil, "UnmarshalResponse for %d", i)
+	}
+
+	exp := NewExp(call)
+	b, err := json.Marshal(exp)
+	require.NoError(t, err, "Marshal EXP")
+	_, err = Unmarshal(bytes.NewReader(b))
+	assert.Error(t, err, "Unmarshal not allowed for EXP")
+	assert.Contains(t, err.Error(), "unknown message EXP", "Unmarshal for EXP returns expected error")
+}
 
 func TestUnmarshalOK(t *testing.T) {
 	raw := `{"meta":{"type":8,"uuid":"845ca2a4-7d1e-44eb-b1b1-304bb222a2bf"},"payload":{"for":"9dc9b548-3a7b-40d0-82b0-ccf7e9828e78","for_type":1}}`
