958fa5d348b02f1e18361849a88e28aa758fbba7
Martin Angers <martin.n.angers@gmail.com>
2016-03-15 09:13:30 -0400

    juggler: refine juggler-server command, vendor yaml

--- juggler/cmd/juggler-server/main.go
+++ juggler/cmd/juggler-server/main.go
@@ -5,9 +5,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"time"
+
+	"gopkg.in/yaml.v2"
 
 	"github.com/PuerkitoBio/exp/juggler"
 	"github.com/PuerkitoBio/exp/juggler/broker/redisbroker"
@@ -16,17 +21,75 @@ import (
 )
 
 var (
-	// TODO : work out a config file for all server and broker options
-	redisAddrFlag       = flag.String("redis-addr", ":6379", "redis address to connect to")
-	allowEmptyProtoFlag = flag.Bool("allow-empty-subprotocol", false, "if set, allow empty subprotocol handshake")
-	portFlag            = flag.Int("port", 9000, "port to listen on")
-	readLimitFlag       = flag.Int("read-limit", 4096, "read message size limit")
-	readTOFlag          = flag.Duration("read-timeout", 10*time.Second, "read deadline duration")
-	writeTOFlag         = flag.Duration("write-timeout", 10*time.Second, "write deadline duration")
+	redisAddrFlag       = flag.String("redis", ":6379", "Redis `address`.")
+	allowEmptyProtoFlag = flag.Bool("allow-empty-subprotocol", false, "Allow empty subprotocol during handshake.")
+	portFlag            = flag.Int("port", 9000, "Server `port`.")
+	configFlag          = flag.String("config", "", "Path of the configuration `file`.")
+	helpFlag            = flag.Bool("help", false, "Show help.")
 )
+
+// Redis defines the redis-specific configuration options.
+type Redis struct {
+	Addr        string        `yaml:"addr"`
+	MaxActive   int           `yaml:"max_active"`
+	MaxIdle     int           `yaml:"max_idle"`
+	IdleTimeout time.Duration `yaml:"idle_timeout"`
+}
+
+// Config defines the configuration options of the server.
+type Config struct {
+	Redis *Redis `yaml:"redis"`
+}
+
+func getConfigFromReader(r io.Reader) (*Config, error) {
+	// set default values
+	conf := &Config{
+		Redis: &Redis{
+			Addr:      *redisAddrFlag,
+			MaxActive: 100,
+			MaxIdle:   10,
+		},
+	}
+
+	if r != nil {
+		b, err := ioutil.ReadAll(r)
+		if err != nil {
+			return nil, err
+		}
+		if err := yaml.Unmarshal(b, conf); err != nil {
+			return nil, err
+		}
+	}
+	return conf, nil
+}
+
+func getConfigFromFile(file string) (*Config, error) {
+	var r io.Reader
+	if file != "" {
+		f, err := os.Open(file)
+		if err != nil {
+			return nil, err
+		}
+		defer f.Close()
+
+		r = f
+	}
+	return getConfigFromReader(r)
+}
 
 func main() {
 	flag.Parse()
+	if *helpFlag {
+		flag.Usage()
+		return
+	}
+
+	conf, err := getConfigFromFile(*configFlag)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to load configuration file: %v\n", err)
+		flag.Usage()
+		os.Exit(1)
+	}
 
 	// wrap LogMsg and ProcessMsg in a PanicRecover handler
 	h := juggler.PanicRecover(
@@ -35,7 +98,7 @@ func main() {
 			juggler.HandlerFunc(juggler.ProcessMsg),
 		), true, true)
 
-	pool := newRedisPool(*redisAddrFlag)
+	pool := newRedisPool(conf.Redis)
 	broker := &redisbroker.Broker{
 		Pool:      pool,
 		Dial:      pool.Dial,
@@ -53,10 +116,8 @@ func main() {
 	}
 	upg := &websocket.Upgrader{Subprotocols: juggler.Subprotocols}
 	srv := &juggler.Server{
-		ReadLimit:               int64(*readLimitFlag),
-		ReadTimeout:             *readTOFlag,
+		ReadLimit:               4096,
 		WriteLimit:              4096,
-		WriteTimeout:            *writeTOFlag,
 		AcquireWriteLockTimeout: 200 * time.Millisecond,
 		ConnState:               juggler.LogConn,
 		Handler:                 h,
@@ -71,11 +132,12 @@ func main() {
 	}
 }
 
-func newRedisPool(addr string) *redis.Pool {
+func newRedisPool(conf *Redis) *redis.Pool {
+	addr := conf.Addr
 	return &redis.Pool{
-		MaxIdle:     10,
-		MaxActive:   10000,
-		IdleTimeout: 2 * time.Minute,
+		MaxIdle:     conf.MaxIdle,
+		MaxActive:   conf.MaxActive,
+		IdleTimeout: conf.IdleTimeout,
 		Dial: func() (redis.Conn, error) {
 			c, err := redis.Dial("tcp", addr)
 			if err != nil {
